dd48a1e3c87778ba9fe27880aa78038727307a1b
Logan <logan.volkers@yupiq.com>
2017-11-08 18:22:19 +0000

    LV: Updated ID generation for custom fields schema

--- updateversion.js
+++ updateversion.js
@@ -3,12 +3,19 @@ const path = require('path');
 const pkg = require("./package.json")
 
 
-const file = "json/SimpleEmail.schema.json";
-const id = `https://unpkg.com/${pkg.name}@${pkg.version}/${file}`
+const files = [
+  "json/SimpleEmail.schema.json",
+  "json/UserCustomFields.schema.json",
+  ];
 
-const filePath = path.resolve(__dirname, file)
+function update(file){
+  const id = `https://unpkg.com/${pkg.name}@${pkg.version}/${file}`
+  const filePath = path.resolve(__dirname, file)
+  return updateJsonFile(filePath, (data) => {
+    data.id = id
+    return data
+  });
+}
 
-return updateJsonFile(filePath, (data) => {
-  data.id = id
-  return data
-}).catch(e => console.log("Error updating JSON file", e))
+
+return Promise.all(files.map(update)).catch(e => console.log("Error updating JSON file", e))
