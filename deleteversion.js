e32338ef0403655c9e5221f909f56f951e5ad7b8
Logan <logan.volkers@yupiq.com>
2017-12-19 21:45:32 +0000

    LV: Added program template schema, added validation tests

---
+++ deleteversion.js
@@ -0,0 +1,23 @@
+const updateJsonFile = require('update-json-file');
+const path = require('path');
+const pkg = require("./package.json")
+
+
+const files = [
+  "SimpleEmail.schema.json",
+  "HtmlEmail.schema.json",
+  "ProgramTemplate.schema.json",
+  "UserCustomFields.schema.json",
+  ];
+
+function update(file){
+  const id = `https://unpkg.com/${pkg.name}@${pkg.version}/json/${file}`
+  const filePath = path.resolve(__dirname, `./json/`, file)
+  return updateJsonFile(filePath, (data) => {
+    data.id = `${file}#`;
+    return data
+  });
+}
+
+
+return Promise.all(files.map(update)).catch(e => console.log("Error updating JSON file", e))
