929b6138c5c4ef95ecb690a5e9900e27d98bbb22
jemason <eric.mason@yupiq.com>
2018-07-19 09:57:32 -0700

    EM added pinterest image URL as option

--- tests/HtmlWidget.test.ts
+++ tests/HtmlWidget.test.ts
@@ -1,13 +1,13 @@
 import * as Ajv from 'ajv';
 
-describe("UserTokenClaims", ()=>{
-
+describe("HtmlWidget", ()=>{
+    
     const ajv = new Ajv();
     ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
-    var validate = ajv.compile(require("../json/UserTokenClaims.schema.json"));
-    
-    ['UserReadToken-v1', "UserUnauthedToken-v2", "UserWriteToken-v1"].map( file =>{
+    var validate = ajv.compile(require("../json/HtmlWidget.schema.json"));
+
+    ['htmlwidget/valid-vanilla'].map( file =>{
         test("Test File:" + file, ()=>{
             var valid = validate(require("../examples/" + file + ".json"));
             if (!valid) expect(validate.errors).toBeNull();
