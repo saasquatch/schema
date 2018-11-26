f98f3fe986a8c77df147ba93aad21963a7c0747a
jericmason <eric.mason@yupiq.com>
2018-11-26 11:44:14 -0800

    EM - a few schema tests for program transaction

--- helpers/Validator.ts
+++ helpers/Validator.ts
@@ -3,14 +3,26 @@ import Ajv from 'ajv';
 const ajv = new Ajv();
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
-export function isValidForSchema(schemaPath: string, ... testInstanceFiles: string[]) {
+function validate(checkValid: boolean, schemaPath: string, ... testInstanceFiles: string[]) {
     var validate = ajv.compile(require(schemaPath));
     
     testInstanceFiles.map( file =>{
         test("Test File:" + file, ()=>{
             var valid = validate(require("../examples/" + file + ".json"));
-            if (!valid) expect(validate.errors).toBeNull();
-            expect(valid).toBeTruthy();
+            if(checkValid) {
+                if (!valid) expect(validate.errors).toBeNull();
+                expect(valid && checkValid).toBeTruthy();
+            } else {
+                expect(valid).toBeFalsy();
+            }
         });
-    });
+    });    
+}
+
+export function isInvalidForSchema(schemaPath: string, ... testInstanceFiles: string[]) {
+    validate(false, schemaPath, ... testInstanceFiles);
+}
+
+export function isValidForSchema(schemaPath: string, ... testInstanceFiles: string[]) {
+    validate(true, schemaPath, ... testInstanceFiles);
 }
