ab074dba095c520da8d0037544d723b1a6b0199e
jericmason <eric.mason@yupiq.com>
2018-12-04 10:12:49 -0800

    EM it's breaking tests - add it in later

--- tests/ProgramTemplate.test.ts
+++
@@ -1,8 +0,0 @@
-import { isValidForSchema, isInvalidForSchema } from '../helpers/Validator';
-
-describe("ProgramTemplate", () => {
-    const schemaPath: string = "../json/ProgramTemplate.schema.json";
-    // TODO - we can't validate these until we remove invalid schema attributes like '"format" : tabs', but some forms may still rely on those
-    // isValidForSchema(schemaPath,
-    //     "programtemplates/Partner");
-});
