3cae6f803f52d158ff53d6455c481239c62da6fa
jericmason <eric.mason@yupiq.com>
2018-12-17 17:39:27 -0800

    EM more schema tests

--- tests/UserMetric.test.ts
+++ tests/UserMetric.test.ts
@@ -1,5 +1,5 @@
 import {isValidForSchema} from '../helpers/Validator';
 
 describe("UserMetric", ()=>{
-    isValidForSchema("../json/UserMetric.schema.json", "usermetric/VIPMetric");
+    isValidForSchema("../json/UserMetric.schema.json", "usermetric/PartnersFirstPurchase", "usermetric/VIPMetricCurrency", "usermetric/VIPMetricNoCurrency");
 })
