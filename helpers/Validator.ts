4d9f00f4b55df8601fc43817ecd065b5f33337e0
jericmason <eric.mason@yupiq.com>
2018-12-18 11:09:15 -0800

    EM formatting

--- helpers/Validator.ts
+++ helpers/Validator.ts
@@ -3,26 +3,26 @@ import Ajv from 'ajv';
 const ajv = new Ajv();
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
-function validate(checkValid: boolean, schemaPath: string, ... testInstanceFiles: string[]) {
+function validate(checkValid: boolean, schemaPath: string, ...testInstanceFiles: string[]) {
     var validate = ajv.compile(require(`../json/${schemaPath}`));
-    
-    testInstanceFiles.map( file =>{
-        test("Test File:" + file, ()=>{
+
+    testInstanceFiles.map(file => {
+        test("Test File:" + file, () => {
             var valid = validate(require(`../examples/${file}.json`));
-            if(checkValid) {
+            if (checkValid) {
                 if (!valid) expect(validate.errors).toBeNull();
                 expect(valid && checkValid).toBeTruthy();
             } else {
                 expect(valid).toBeFalsy();
             }
         });
-    });    
+    });
 }
 
-export function isInvalidForSchema(schemaPath: string, ... testInstanceFiles: string[]) {
-    validate(false, schemaPath, ... testInstanceFiles);
+export function isInvalidForSchema(schemaPath: string, ...testInstanceFiles: string[]) {
+    validate(false, schemaPath, ...testInstanceFiles);
 }
 
-export function isValidForSchema(schemaPath: string, ... testInstanceFiles: string[]) {
-    validate(true, schemaPath, ... testInstanceFiles);
+export function isValidForSchema(schemaPath: string, ...testInstanceFiles: string[]) {
+    validate(true, schemaPath, ...testInstanceFiles);
 }
