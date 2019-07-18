9fb0d2cf7f50486060ee008ef2c66c3724328f34
jericmason <eric.mason@yupiq.com>
2019-07-18 10:34:52 -0700

    EM cleanup

--- tests/UserTokenClaims.test.ts
+++ tests/UserTokenClaims.test.ts
@@ -1,5 +1,5 @@
 import {isValidForSchema} from '../helpers/Validator';
- 
+
 describe("UserTokenClaims", ()=>{
     isValidForSchema("UserTokenClaims.schema.json", "UserReadToken-v1", "UserUnauthedToken-v2", "UserWriteToken-v1");
 })
