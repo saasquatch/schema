44cbcd25c20b06f5419f675b6178e4642e279a97
jericmason <eric.mason@yupiq.com>
2019-08-29 12:32:28 -0700

    EM added in integration template schema

---
+++ tests/IntegrationTemplate.test.ts
@@ -0,0 +1,7 @@
+import {isValidForSchema, isInvalidForSchema} from '../helpers/Validator';
+
+describe("IntegrationTemplate", ()=>{
+    const schemaPath: string = "IntegrationTemplate.schema.json";
+    isValidForSchema(schemaPath, 
+        "integrationconfig/ValidIntegration");
+})
