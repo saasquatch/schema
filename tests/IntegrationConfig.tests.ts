2ea0cf7129187b4731577cce56ac6752dbcf7b2a
jericmason <eric.mason@yupiq.com>
2019-08-29 10:29:46 -0700

    EM added integration config schema

---
+++ tests/IntegrationConfig.tests.ts
@@ -0,0 +1,9 @@
+import {isValidForSchema, isInvalidForSchema} from '../helpers/Validator';
+
+describe("IntegrationConfig", ()=>{
+    const schemaPath: string = "IntegrationConfig.schema.json";
+    isValidForSchema(schemaPath, 
+        "integrationconfig/ValidWebhook");
+    isInvalidForSchema(schemaPath,
+        "integrationconfig/InvalidWebhook");
+})
