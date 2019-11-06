f7d84551b80a97080e191475a4dff8efa1d4f9f9
jericmason <eric.mason@yupiq.com>
2019-11-05 17:18:01 -0800

    Update src/json/program/AfterUserEventProcessedTriggerContext.schema.ts
    Co-Authored-By: Logan Volkers <logan@saasquat.ch>

--- src/json/program/AfterUserEventProcessedTriggerContext.schema.ts
+++ src/json/program/AfterUserEventProcessedTriggerContext.schema.ts
@@ -8,7 +8,7 @@ import {
 const afterUserEventProcessedTriggerContextSchema: JSONSchema6 = {
   $schema: "http://json-schema.org/draft-06/schema#",
   type: "object",
-  title: "After User Event Processed Trigger Context",
+  title: "Event Trigger Variables",
   description:
     "Defines the trigger variables available to an AFTER_USER_EVENT_PROCESSED trigger",
   properties: {
