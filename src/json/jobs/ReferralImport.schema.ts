bb22ef2ef89f16756327bb16d6906e527e47f6b6
jericmason <eric.mason@yupiq.com>
2020-01-23 17:23:15 -0800

    EM playing around with merging the schema properties

--- src/json/jobs/ReferralImport.schema.ts
+++ src/json/jobs/ReferralImport.schema.ts
@@ -1,4 +1,5 @@
 import { JSONSchema6 } from "json-schema";
+import mergeJson from "merge-json";
 import rewardApiInput from "../RewardApiInput.schema.json";
 import userIdentifier from "../UserIdentifier.schema.json";
 
@@ -40,42 +41,22 @@ const referralImport: JSONSchema6 = {
       type: "array",
       title: "Rewards",
       items: {
-        ...(<JSONSchema6>rewardApiInput).properties
-        // type: "object",
-        // properties: {
-        //   id: {
-        //     type: "string",
-        //     title: "User Id"
-        //   },
-        //   type: {
-        //     type: "string",
-        //     title: "Reward Type",
-        //     enum: ["CREDIT"]
-        //   },
-        //   accountId: {
-        //     type: "string",
-        //     title: "Account Id"
-        //   },
-        //   unit: {
-        //     type: "string",
-        //     title: "Reward Unit"
-        //   },
-        //   assignedCredit: {
-        //     type: "string",
-        //     title: "Reward Amount"
-        //   },
-        //   programRewardKey: {
-        //     type: "string",
-        //     title: "Reward Key"
-        //   },
-        //   dateGiven: {
-        //     type: "integer",
-        //     title: "Date Given"
-        //   },
-        //   redemptions: {
-        //     $ref: "#/definitions/redemptions"
-        //   }
-        // }
+        ...mergeJson.merge((<JSONSchema6>rewardApiInput).properties, {
+          type: "object",
+          properties: {
+            programRewardKey: {
+              type: "string",
+              title: "Reward Key"
+            },
+            dateGiven: {
+              type: "integer",
+              title: "Date Given"
+            },
+            redemptions: {
+              $ref: "#/definitions/redemptions"
+            }
+          }
+        })
       }
     },
     redemptions: {
