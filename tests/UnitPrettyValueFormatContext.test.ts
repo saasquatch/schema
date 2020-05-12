885ae40ee844212e617e70bf99058f527edb2833
slisaasquatch <slisaasquatch@users.noreply.github.com>
2020-05-12 10:41:47 -0700

    global reward config follow-up (#25)
    * SL: add pretty value format to unit settings
    
    * Update UnitSettings.schema.json
    
    * Update src/json/UnitSettings.schema.json
    
    Co-Authored-By: Logan Volkers <logan@saasquat.ch>
    
    * Update src/json/UnitSettings.schema.json
    
    Co-Authored-By: Logan Volkers <logan@saasquat.ch>
    
    * SL: add name field
    
    * 1.13.4-0
    
    * SL: add supportsCurrencies
    
    * Update ValidUnitSettings.json
    
    * Update ValidUnitSettings.json
    
    * Update UnitSettings.schema.json
    
    * Update UnitSettings.schema.json
    
    * 1.13.4-1
    
    * Create UnitPrettyValueFormatContext.schema.json
    
    * 1.14.0
    
    * SL: currency pattern
    
    * Update UnitPrettyValueFormatContext.schema.json
    
    * Update UnitPrettyValueFormatContext.schema.json
    
    * SL: add basic test
    
    * SL: test examples
    
    * SL: test example
    
    * SL: test case
    
    * SL: test cases
    
    * SL: ease numberToBasic restriction
    
    * 1.14.1-0
    
    * 1.14.1
    
    Co-authored-by: Logan Volkers <logan@saasquat.ch>

---
+++ tests/UnitPrettyValueFormatContext.test.ts
@@ -0,0 +1,20 @@
+import { isValidForSchema, isInvalidForSchema } from "../helpers/Validator";
+
+describe("UnitSettings", () => {
+  isValidForSchema(
+    "UnitPrettyValueFormatContext.schema.json",
+    ...rangeFrom0(3).map(
+      (idx) => `unitsettings/ValidUnitPrettyValueFormatContext${idx}`
+    )
+  );
+  isInvalidForSchema(
+    "UnitPrettyValueFormatContext.schema.json",
+    ...rangeFrom0(4).map(
+      (idx) => `unitsettings/InvalidUnitPrettyValueFormatContext${idx}`
+    )
+  );
+});
+
+function rangeFrom0(count: number) {
+  return Array.from(new Array(count), (x, i) => i);
+}
