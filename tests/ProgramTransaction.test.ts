40753a3d9a9b373c21f12c5fcafa41006fc8bc2c
slisaasquatch <sli@saasquat.ch>
2021-01-14 16:08:14 -0800

    SL: update tests to match

--- tests/ProgramTransaction.test.ts
+++ tests/ProgramTransaction.test.ts
@@ -2,14 +2,14 @@ import {isValidForSchema, isInvalidForSchema} from '../helpers/Validator';
 
 describe("ProgramTransactions", ()=>{
     const schemaPath: string = "ProgramTransaction.schema.json";
-    isValidForSchema(schemaPath, 
-        "programtransactions/ProgramTransactionDynamicReward", 
+    isValidForSchema(schemaPath,
+        "programtransactions/ProgramTransactionDynamicReward",
         "programtransactions/ProgramTransactionOverrideReward",
         "programtransactions/ProgramTransactionModeration",
-        "programtransactions/ProgramTransactionValidAnalytic",
+        "programtransactions/ProgramTransactionValidAnalytic1",
+        "programtransactions/ProgramTransactionValidAnalytic2",
         "programtransactions/ProgramTransactionGoalAnalytic");
     isInvalidForSchema(schemaPath,
         "programtransactions/ProgramTransactionDynamicOverrideReward",
-        "programtransactions/ProgramTransactionInvalidAnalytic1",
-        "programtransactions/ProgramTransactionInvalidAnalytic2");
+        "programtransactions/ProgramTransactionInvalidAnalytic1");
 })
