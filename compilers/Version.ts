cae654003c064c329b6f2a8122ce956836db3c91
Jayden Chan <jayden@jayden.codes>
2025-05-07 16:06:14 -0600

    refactor: no need for ancient Node compatibility

--- compilers/Version.ts
+++ compilers/Version.ts
@@ -1,9 +1,5 @@
 import RefParser from "@apidevtools/json-schema-ref-parser";
-import {
-  mkdir as mkdirCb,
-  readFile as readFileCb,
-  writeFile as writeFileCb,
-} from "fs";
+import { mkdir, readFile, writeFile } from "fs/promises";
 import glob from "glob";
 import { JSONSchema6 } from "json-schema";
 import path from "path";
@@ -11,45 +7,8 @@ import path from "path";
 const SCHEMAS_GLOB = "**/*.schema.@(json|ts)";
 const SCHEMAS_FOLDER = path.resolve(__dirname, "../src/json", SCHEMAS_GLOB);
 
-const mkdir = (path: string): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    mkdirCb(path, { recursive: true }, (err, path) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(path);
-      }
-    });
-  });
-};
-
-const readJsonFile = (path: string): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    readFileCb(path, { encoding: "utf8" }, (err, contents) => {
-      if (err) {
-        reject(err);
-      } else {
-        try {
-          const ret = JSON.parse(contents);
-          resolve(ret);
-        } catch (e) {
-          reject(e);
-        }
-      }
-    });
-  });
-};
-
-const writeFile = (path: string, contents: string): Promise<void> => {
-  return new Promise<void>((resolve, reject) => {
-    writeFileCb(path, contents, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+const readJsonFile = async (path: string): Promise<any> => {
+  return JSON.parse(await readFile(path, { encoding: "utf8" }));
 };
 
 // Combine the program rules schema with the rest of the program template JSON
@@ -83,7 +42,7 @@ async function resolveReferences(
 ): Promise<void> {
   const refParser = new RefParser();
   const parsedExternalRefs = await refParser.bundle(filePath);
-  await mkdir(path.dirname(outputPath));
+  await mkdir(path.dirname(outputPath), { recursive: true });
   await writeFile(outputPath, JSON.stringify(parsedExternalRefs, null, 2));
   return;
 }
